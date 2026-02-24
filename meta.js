3b62d0acfff73d7a4e676eaec0eaa5170c74901c
Samy Ahmed <152093199+Samyahmed480@users.noreply.github.com>
2026-02-24 21:43:50 +0200

    Update meta.js

--- meta.js
+++ meta.js
@@ -3,8 +3,8 @@ const metaTag = document.querySelector('meta[name="client_code"]');
 const clientCode = metaTag ? metaTag.getAttribute('content') : null;
 
 // 2. إعداد روابط التحويل (غيرها بروابطك الحقيقية)
-const externalWebsite = "https://samyahmed480.github.io/su-s/"; // لو الاشتراك انتهى
-const wrongCodePage = "https://samyahmed480.github.io/su-s/"; // لو الكود مسروق أو مش موجود
+const externalWebsite = "https://samyahmed480.github.io/su-m/"; // لو الاشتراك انتهى
+const wrongCodePage = "https://samyahmed480.github.io/su-m/"; // لو الكود مسروق أو مش موجود
 
 // 3. رابط الـ API بتاع شيت جوجل الخاص بيك
 const googleSheetURL = "https://script.google.com/macros/s/AKfycbxV48WX5qZQM5sMMRH2HI5uDonVZTqc1maeMKkJxq8/dev";
